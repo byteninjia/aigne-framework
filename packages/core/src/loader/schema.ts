bd80921bbbe8385645eb7c52fd719ce48d672da9
Yechao LI <liyechao@yechao.xyz>
2025-07-14 14:09:24 +0800

    fix(core): fix error of external schema with array type (#251)

--- packages/core/src/loader/schema.ts
+++ packages/core/src/loader/schema.ts
@@ -3,6 +3,43 @@ import { parse } from "yaml";
 import { type ZodType, z } from "zod";
 
 export const inputOutputSchema = ({ path }: { path: string }) => {
+  const includeExternalSchema = async (schema: any): Promise<typeof schema> => {
+    if (schema?.type === "object" && schema.properties) {
+      return {
+        ...schema,
+        properties: Object.fromEntries(
+          await Promise.all(
+            Object.entries(schema.properties).map(async ([key, value]) => [
+              key,
+              await includeExternalSchema(value),
+            ]),
+          ),
+        ),
+      };
+    }
+
+    if (schema?.type === "array" && schema.items) {
+      return { ...schema, items: await includeExternalSchema(schema.items) };
+    }
+
+    // Load nested schemas from file
+    if (typeof schema === "string") {
+      const raw = await nodejs.fs.readFile(
+        nodejs.path.join(nodejs.path.dirname(path), schema),
+        "utf8",
+      );
+      return nestedJsonSchema.parseAsync(parse(raw));
+    }
+    return schema;
+  };
+
+  const nestedJsonSchema = z
+    .object({
+      type: z.string(),
+    })
+    .passthrough()
+    .transform((v) => includeExternalSchema(v));
+
   const jsonSchemaSchema = z
     .object({
       type: z.literal("object"),
@@ -10,39 +47,7 @@ export const inputOutputSchema = ({ path }: { path: string }) => {
       required: z.array(z.string()).optional(),
       additionalProperties: z.boolean().optional(),
     })
-    .transform((v) => {
-      const t = async (schema: any): Promise<typeof schema> => {
-        if (schema?.type === "object" && schema.properties) {
-          return {
-            ...schema,
-            properties: Object.fromEntries(
-              await Promise.all(
-                Object.entries(schema.properties).map(async ([key, value]) => [
-                  key,
-                  await t(value),
-                ]),
-              ),
-            ),
-          };
-        }
-
-        if (schema?.type === "array" && schema.items) {
-          return { ...schema, items: await t(schema.items) };
-        }
-
-        // Load nested schemas from file
-        if (typeof schema === "string") {
-          const raw = await nodejs.fs.readFile(
-            nodejs.path.join(nodejs.path.dirname(path), schema),
-            "utf8",
-          );
-          return jsonSchemaSchema.parseAsync(parse(raw));
-        }
-        return schema;
-      };
-
-      return t(v);
-    });
+    .transform((v) => includeExternalSchema(v));
 
   return z.union([
     z
