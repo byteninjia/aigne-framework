fa5e427eb29157c3ebcd9c9bf8c5c6b31efad4ae
Yechao LI <liyechao@yechao.xyz>
2025-08-21 09:36:27 +0800

    fix(cli): force upgrade app while load app error (#390)

--- packages/cli/src/commands/app.ts
+++ packages/cli/src/commands/app.ts
@@ -162,14 +162,21 @@ export async function loadApplication({
   name = `@aigne/${name}`;
   dir ??= join(homedir(), ".aigne", "registry.npmjs.org", name);
 
-  const check = forceUpgrade ? undefined : await isInstallationAvailable(dir);
+  let check = forceUpgrade ? undefined : await isInstallationAvailable(dir);
   if (check?.available) {
-    return {
-      aigne: await AIGNE.load(dir),
-      dir,
-      version: check.version,
-      isCache: true,
-    };
+    const aigne = await AIGNE.load(dir).catch((error) => {
+      console.warn(`Failed to load ${name}, trying to reinstall:`, error.message);
+    });
+    if (aigne) {
+      return {
+        aigne,
+        dir,
+        version: check.version,
+        isCache: true,
+      };
+    }
+
+    check = undefined;
   }
 
   const result = await new Listr<{
